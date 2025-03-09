d9402a9ce9ff19162508300675aacacd870c2e0e
dewibatista <62663654+dewi-batista@users.noreply.github.com>
2025-03-09 02:01:07 +0100

    some changes

--- mlp.py
+++ mlp.py
@@ -37,6 +37,14 @@ def train():
         if (epoch + 1) % 10 == 0:
             print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}")
 
+# let the testing beginnnnnn
+def test():
+    model.eval()
+    with torch.no_grad():
+        y_pred_test = model(X_test)
+        test_mse = criterion(y_pred_test, y_test).item()
+    print(f"Test RMSE: {np.sqrt(test_mse)}")
+
 if __name__ == "__main__":
 
     # load data
@@ -50,20 +58,22 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # pytorch tensor bullshit
-    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
-    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
 
     # pytorch dataloader bullshit
-    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+    train_dataset = TensorDataset(X_train, y_train)
+    test_dataset = TensorDataset(X_test, y_test)
+    
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     # initialise model
-    input_size = X_train_tensor.shape[1]
-    output_size = y_train_tensor.shape[1]
+    input_size = X_train.shape[1]
+    output_size = y_train.shape[1]
     model = MLP_model(input_size, output_size)
 
     # some hyperparams
@@ -71,15 +81,9 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     num_epochs = 100
 
-    # train
+    # train and test
     train()
-
-    # let the testing beginnnnnn
-    model.eval()
-    with torch.no_grad():
-        y_pred_test = model(X_test_tensor)
-        test_mse = criterion(y_pred_test, y_test_tensor).item()
-    print(f"Test RMSE: {np.sqrt(test_mse)}")
+    test()
 
     # empirical testing
     model.eval()
