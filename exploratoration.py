6c5b4c20349d24c49f9f5fa5f3704cbe7a1662c0
dewibatista <62663654+dewi-batista@users.noreply.github.com>
2025-03-09 16:44:16 +0100

    some changes

--- exploratoration.py
+++ exploratoration.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 
 # load data
-data = pd.read_csv('data/augmented_mixtures.csv')
+data = pd.read_csv('data/preproc_mixtures.csv')
 
 # # histograms and density plots to check distribution shape
 # for col in data.columns[1 : -1]:
@@ -14,16 +14,43 @@ data = pd.read_csv('data/augmented_mixtures.csv')
 #     plt.ylabel('Freq.')
 #     plt.show()
 
-# correlations condition on:
-data_blood = data[data['Blood'] == 1]
+fluid_combinations = [
+    ('Semen.fertile', 'Vaginal.mucosa'),
+    # ('Saliva', 'Vaginal.mucosa'),
+    # ('Blood', 'Nasal.mucosa'),
+    # ('Nasal.mucosa', 'Saliva'),
+    # ('Blood', 'Vaginal.mucosa'),
+    # ('Blood', 'Menstrual.secretion')
+]
 
-# blelele
+# set starting index of markers in csv
 marker_start_index = list(data.columns).index("HBB")
 markers = data.columns[marker_start_index :]
-corr_matrix = data_blood[markers].corr()
 
-# plot that correlation matrix boiiii
-plt.figure(figsize=(10, 8))
-sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
-plt.title("Correlation Matrix for Marker Values (Blood = 1)")
-plt.show()
+# compute correlations for all possible fluid couples
+max_num_correlates = 2
+corr_threshold = 0.2
+for (fluid_1, fluid_2) in fluid_combinations:
+    
+    # restrict dataset to just the rows where these two fluids appear
+    restricted_data = data[(data[fluid_1] == 1) & (data[fluid_2] == 1)]
+
+    # compute correlation matrix
+    corr = restricted_data[markers].corr()
+
+    print()
+    print(fluid_1, fluid_2)
+
+    # get markers that best correlate (non-negligibly) with each other marker
+    for marker in markers:
+        top_n_correlates = corr.loc[marker].abs().nlargest(max_num_correlates + 1)
+        top_n_correlates = top_n_correlates[top_n_correlates > corr_threshold].index.tolist()
+        if marker in top_n_correlates: # get rid of marker (correlates with itself)
+            top_n_correlates.remove(marker)
+        print(marker, top_n_correlates)
+
+    # plot that correlation matrix boiiii
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(corr, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+    plt.title("Correlation Matrix")
+    plt.show()
