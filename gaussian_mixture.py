0458831c528f7ce27e1a0503e71a98ac74fc736b
Andrei Secuiu <a.secuiu@student.rug.nl>
2025-03-10 12:45:22 +0100

    Updated gaussian_mixture.py
    Added automatic model selection
    Created data generation tool
    Added KS function to test equality of distributions

--- gaussian_mixture.py
+++ gaussian_mixture.py
@@ -1,23 +1,26 @@
+# Good for all
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
-from scipy.stats import multivariate_normal
+# For stats things
+from scipy.stats import multivariate_normal, ks_2samp
 from sklearn.mixture import GaussianMixture
 
+# Python quality of life
 from pdb import set_trace
-
-#' Try to obtain the peak heights distribution as a mixture of Gaussians. 
-#' Try to obtain it for simple cases first: one sample type, one marker
-#' Then try to generalize to multiple mixture types
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 def gauss_mixture_fit(data_set, idx_marker, idx_sample, 
                        n_components=1, covariance_type="diag",
-                       plot_=True, do_model_selection=True):
+                       plot_=True, do_model_selection=False):
     """
     This function fits a mixture of Gaussians. If enabled, it will plot the histogram overlapped
     with the fitted distribution, as well as perform model selection for the optimal number of components.
+
+    The documentation for GaussianMixture: 
+    https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html#sklearn.mixture.GaussianMixture
 
     --- Inputs:
 
@@ -25,11 +28,34 @@ def gauss_mixture_fit(data_set, idx_marker, idx_sample,
     Otherwise it uses the mixtures
 
     idx_marker = integer. The index of the relevant marker in the list of markers from the data
-    The list:
+    The list: [HBB,ALAS2,CD93,HTN3,STATH,BPIFA1,MUC4,MYOZ1,CYP2B7P1,MMP10,MMP7,MMP11,SEMG1,KLK3,PRM1]
 
-    - TBD: 
+    idx_sample = integer. The index of the relevant fluid type in the list of fluids. The list:
+    - individuals: ['Blood' 'Saliva' 'Vaginal.mucosa' 'Menstrual.secretion' 'Semen.fertile'
+    'Semen.sterile' 'Nasal.mucosa' 'Blank_PCR' 'Skin' 'Skin.penile']
+    - mixtures: ['Semen.fertile+Vaginal.mucosa' 'Saliva+Semen.fertile'
+    'Saliva+Vaginal.mucosa' 'Blood+Nasal.mucosa' 'Nasal.mucosa+Saliva'
+    'Vaginal.mucosa+Blood' 'Menstrual.secretion+Blood']
 
-    Implement model selection per number of components
+    n_components = integer. The number of components to be used for the Gaussian mixture, set by the user.
+    Relevant only if 'do_model_selection' is set to False
+
+    covariance_type = string. Choose between ["diag", "full", "spherical", "tied"]. It gives the 
+    type of matrix to be used in the mixture definitions. 
+
+    plot_ = boolean. If true, it also plots a histogram of the relevant data together with the best mixture fit
+
+    do_model_selection = boolean. If true, it performs model selection to find the best number of mixture components.
+    **Note**: it uses BIC, which is the stricter criterion. It also has a maximum mixture number of 10. If those need to 
+    be updated, this is an avenue of improvement
+
+
+    --- Output:
+
+    model = model class of Gaussian Mixtures. It returns the model with the best number of components (user specified or via BIC)
+
+    --- TBD: 
+
     Implement data generation
     Test via the KS test the adequacy of the fit
     """
@@ -44,15 +70,27 @@ def gauss_mixture_fit(data_set, idx_marker, idx_sample,
     samples = data.iloc[:, 0].unique()
 
     data_sample = data[data.iloc[:, 0] == samples[idx_sample]]
+    X = data_sample.loc[:, [markers[idx_marker]]]
 
-    # - Fit a mixture of gaussians
-    
-    #' Include model selection per number of components
-    #' BIC favours simpler models, AIC
+    if do_model_selection == False:
+        model = GaussianMixture(n_components=n_components, covariance_type=covariance_type, 
+                                n_init=3, random_state=0).fit(X) 
+        print(f"Number of components: {n_components}")
+    else:
+        max_components = 10
+        BIC_list = np.zeros(max_components)
 
+        for i in range(1, max_components + 1):
+            model_temp = GaussianMixture(n_components=i, covariance_type=covariance_type, 
+                                n_init=3, random_state=0).fit(X) 
+            BIC_list[i-1] = model_temp.bic(X)
 
-    model = GaussianMixture(n_components=n_components, covariance_type=covariance_type, 
-                            n_init=3, random_state=0).fit(data_sample.loc[:, [markers[idx_marker]]])
+        # Select the model with the smallest BIC
+
+        best_no_comp = np.argmin(BIC_list) + 1
+        model = GaussianMixture(n_components=best_no_comp, covariance_type=covariance_type, 
+                                n_init=3, random_state=0).fit(X)
+        print(f"Number of components: {best_no_comp}") 
 
     print(f"Convergence status: {model.converged_}")
 
@@ -93,34 +131,124 @@ def gauss_mixture_fit(data_set, idx_marker, idx_sample,
         plt.plot(x_val, y_all, color="blue", label="Sum Contrib")
 
         for i in range(len(model.weights_)):
-            plt.plot(x_val, y_val[i, :], label=f"Component {i}")
+            plt.plot(x_val, y_val[i, :], label=f"Component {i+1}")
 
         plt.title(f'{samples[idx_sample]} - {markers[idx_marker]}\nTrue data and fit')
         plt.xlabel('Value')
         plt.ylabel('Freq.')
         plt.legend()
         plt.show()
+    
+    return model
+
+def data_generator(n, model, covariance_type="diag", threshold=150, seed=42):
+    """
+    This function generates data artificially, following the Gaussian mixture model from 'model'.
+    The number of samples generated is 'n'
+    The function also implements a thresolding mechanic: if a generated sample would be below 150, it is 
+    returned as zero
+    """
+
+    np.random.seed(seed)
+
+    # Initialize
+    weights = model.weights_
+    new_samples = np.zeros(n)
+    temp1 = 0
+
+    # Sample mixture components to take from 
+    temp2 = np.random.choice(range(len(weights)), size=n, p=weights)
+    mixture_number = np.bincount(temp2) 
+
+    # Sample values from mixtures
+    for i in range(len(weights)):
+        # Extract curve parameters for relevant mixture component
+        mean_ = model.means_[i]
+
+        if covariance_type == "diag":
+            covar_ = np.diag(model.covariances_[i])
+        elif covariance_type == "full":
+            covar_ = model.covariances_[i] 
+        elif covariance_type == "spherical":
+            covar_ = model.covariances_[i]
+        else:
+            covar_ = model.covariances_
+        
+        # Sample
+        new_samples[temp1:(temp1+mixture_number[i])] = multivariate_normal.rvs(mean=mean_, cov=covar_, size=mixture_number[i])
+        temp1 = temp1 + mixture_number[i]
+
+    # Do thresholding
+    new_samples[new_samples <= threshold] = 0
+
+    return new_samples
 
 if __name__ == "__main__":
-    # --- Print the available choices
+    # --- Test mixture fitting:
 
-    #' Mixed samples: ['Semen.fertile+Vaginal.mucosa' 'Saliva+Semen.fertile'
-    #'  'Saliva+Vaginal.mucosa' 'Blood+Nasal.mucosa' 'Nasal.mucosa+Saliva'
-    #'  'Vaginal.mucosa+Blood' 'Menstrual.secretion+Blood']
+    # - Blood (0), HBB (0)
+    #gauss_mixture_fit("individuals", 0, 0, n_components=3, covariance_type="diag") 
+    #gauss_mixture_fit("individuals", 0, 0, do_model_selection=True, covariance_type="diag")
 
-    #' Individual samples: ['Blood' 'Saliva' 'Vaginal.mucosa' 'Menstrual.secretion' 'Semen.fertile'
-    #' 'Semen.sterile' 'Nasal.mucosa' 'Blank_PCR' 'Skin' 'Skin.penile']
+    # - Saliva (1), STATH (4)
+    #gauss_mixture_fit("individuals", 4, 1, do_model_selection=True, covariance_type="diag")
+    
+    # - Semen.fertile+Vaginal.mucosa (0), MUC4 (6)
+    #gauss_mixture_fit("mixtures", 0, 6, do_model_selection=True, covariance_type="diag")
 
-    #' Markers: [HBB,ALAS2,CD93,HTN3,STATH,BPIFA1,MUC4,MYOZ1,CYP2B7P1,MMP10,MMP7,MMP11,SEMG1,KLK3,PRM1]
+    # --- Test data generation
+    
+    idx_marker = 0
+    idx_sample = 0
+    
+    # - Get data
 
-    # --- Check the first markers for blood and nasal mucosa
+    # Extract true data
+    data = pd.read_csv('data/individuals.csv').fillna(0)
 
-    #' Start with a marker which separates the samples very well: HBB
+    markers = data.columns[1:-5]
+    samples = data.iloc[:, 0].unique()
 
-    idx_marker = 0    # Check HBB
+    data_sample = data[data.iloc[:, 0] == samples[idx_sample]]
+    true_data = data_sample.loc[:, [markers[idx_marker]]]
 
-    gauss_mixture_fit("individuals", idx_marker, 0, n_components=2, covariance_type="diag")
-    gauss_mixture_fit("individuals", idx_marker, 6, n_components=1, covariance_type="diag", plot_=False)
+    # Create new data
+    model = gauss_mixture_fit("individuals", 0, 0, do_model_selection=True, covariance_type="diag", plot_=False)
+    new_data = data_generator(10**2, model, threshold=150)
 
-    #set_trace()
-    gauss_mixture_fit("mixtures", idx_marker, 3, n_components=3, covariance_type="diag")
+    # - Test equality of distributions
+
+    result = ks_2samp(true_data.to_numpy().flatten(), new_data)
+    print(f"p-value for two-sided KS test: {result.pvalue}")
+
+    # - Plot the result 
+
+    # Create the mixture curves
+    x_val = np.arange(data_sample[markers[idx_marker]].min(), data_sample[markers[idx_marker]].max(), 1) 
+    y_val = np.zeros((len(model.weights_), x_val.shape[0]))
+    y_all = np.zeros(x_val.shape)
+
+    # Compute each individual contribution:
+    for i in range(len(model.weights_)):
+        weight_ = model.weights_[i]
+        mean_ = model.means_[i]
+        covar_ = np.diag(model.covariances_[i])
+
+        y_val[i, :] = weight_ * multivariate_normal.pdf(x_val, mean=mean_, cov=covar_)
+        y_all = y_all + y_val[i, :]
+
+    plt.figure(figsize=(6,4))
+
+    plt.hist(new_data, bins=100, label="Generated Data", density=True, color="red", alpha=0.5)
+    plt.hist(true_data, bins=50, label="True Data", density=True, color="blue", alpha=0.5)
+    
+    plt.plot(x_val, y_all, color="black", label="Sum Contrib")
+
+    for i in range(len(model.weights_)):
+        plt.plot(x_val, y_val[i, :], label=f"Component {i+1}")
+
+    plt.title(f'{samples[idx_sample]} - {markers[idx_marker]}\nTrue data and generated data')
+    plt.xlabel('Value')
+    plt.ylabel('Freq.')
+    plt.legend()
+    plt.show()
